da2f63bb498c9d9f6f1a08e4dfc24b0bb19ff4b3
MR-Chouman <choumanmohamadrida@gmail.com>
2024-04-29 09:38:36 +0300

    Controller <> Updated Post Index Method

--- server/app/Http/Controllers/PostController.php
+++ server/app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
 
 class PostController extends Controller
 {
@@ -12,7 +13,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Post::all();
+        return json_encode($posts);
     }
 
     /**

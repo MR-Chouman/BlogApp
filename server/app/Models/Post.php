70296d6011007870c117110e17a845b8506b3398
MR-Chouman <choumanmohamadrida@gmail.com>
2024-04-29 09:03:29 +0300

    Model <> Updated Relation Between Post & User (BelongsTo)

--- server/app/Models/Post.php
+++ server/app/Models/Post.php
@@ -21,4 +21,9 @@ class Post extends Model
     {
         return $this->belongsTo(Category::class);
     }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
